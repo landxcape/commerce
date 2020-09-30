1344eed8c838f67d00e28e131c4a6d51b000b435
Ashish Shakya <38076182+landxcape@users.noreply.github.com>
2020-09-30 09:53:15 +0545

    new models successfully applied

--- auctions/models.py
+++ auctions/models.py
@@ -6,29 +6,25 @@ class User(AbstractUser):
     pass
 
 
-class Bids(models.Model):
-    listing = models.ForeignKey(
-        AuctionListings, on_delete=models.CASCADE, related_name="bids")
-    author = models.ForeignKey(User)
-    amount = models.IntegerField()
-
-
-class Comments(models.Model):
-    listing = models.ForeignKey(
-        AuctionListings, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User)
-    text = models.TextField()
-    created_date = models.DateTimeField(auto_now=True)
-
-
 class AuctionListings(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="listings")
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=300)
     image_url = models.URLField(max_length=200, blank=True, default="")
     category = models.CharField(
         max_length=50, blank=True, default="No Category Listed")
     creation_time = models.DateTimeField(auto_now_add=True, blank=True)
-    bid = models.ForeignKey(
-        Bids, on_delete=models.CASCADE, related_name="listing")
-    comments = models.ForeignKey(
-        Comments, on_delete=models.CASCADE, related_name="listing")
+
+
+class Bids(models.Model):
+    listing_id = models.ForeignKey(
+        AuctionListings, on_delete=models.CASCADE, related_name="bids")
+    amount = models.IntegerField()
+
+
+class Comments(models.Model):
+    listing_id = models.ForeignKey(
+        AuctionListings, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now=True)
